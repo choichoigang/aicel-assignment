5201d85f00f1962709488b957a8065441edc7bc7
choijangho <jhchoi1115@gmail.com>
2020-10-16 03:19:04 +0900

    Add : App.tsx에 ThemeProvider , GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+/* Style */
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+import theme from "./styles/theme";
+
+/* Component */
+import Test from "./components/Test";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Test />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
