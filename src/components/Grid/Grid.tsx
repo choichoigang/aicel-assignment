7ea6f7f2caf5cd102b982bb5fdcf14e3162afc87
choijangho <jhchoi1115@gmail.com>
2020-10-18 22:20:50 +0900

    Feat : Grid Component 전체 Layout 관리 Component 추가

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SectionWrapper } from "./Grid.element";
+import GridTable from "./GridTable";
+
+const Grid = () => {
+  return (
+    <SectionWrapper>
+      <GridTable />
+    </SectionWrapper>
+  );
+};
+
+export default Grid;
