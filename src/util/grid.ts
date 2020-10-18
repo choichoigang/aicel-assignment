8f39b317e419007b31fd8c0854879479e6b330bf
choijangho <jhchoi1115@gmail.com>
2020-10-18 22:22:57 +0900

    Docs : util 함수명 변경

--- src/util/grid.ts
+++ src/util/grid.ts
@@ -1,4 +1,4 @@
-export function changeToLowerCase(value: string | number) {
+export function changeToElement(value: string | number) {
   if (typeof value === "number") return value.toString();
   if (typeof value === "string") return value.toLowerCase();
 }
