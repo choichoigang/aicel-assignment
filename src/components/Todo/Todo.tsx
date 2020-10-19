7a1271ccf8914a0ffbaca33029b2e296ec21509c
choijangho <jhchoi1115@gmail.com>
2020-10-19 21:46:57 +0900

    Feat : Todo Item 생성 제한 조건 추가

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,5 +1,8 @@
 import React, { useContext, useState } from "react";
 
+import { useSelector } from "react-redux";
+import { RootState } from "../../modules/index";
+
 import { ThemeContext } from "styled-components";
 import { TodoWrapper } from "./Todo.element";
 import Button from "../../styles/Button";
@@ -8,11 +11,18 @@ import TodoList from "./TodoList";
 import Portal from "../Portal/Portal";
 import EditTodoModal from "./EditTodoModal";
 
+import { MAX_TODO_LIST } from "../../constants/todo";
+
 function Todo() {
+  const { todoList } = useSelector((state: RootState) => state.todo);
+
   const [isOpen, setIsOpen] = useState(false);
   const { colors } = useContext(ThemeContext);
 
   function handleModalOpen() {
+    if (todoList.length === MAX_TODO_LIST)
+      return alert("최대 5개 이상 생성할 수 없습니다.");
+
     setIsOpen(true);
   }
 
